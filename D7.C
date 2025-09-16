e8709eaa3faf119b00bdc2fc365d1f00d62d51b6
Aditya Ojha <ojha8aditya@gmail.com>
2025-09-16 23:37:21 +0530

    Add files via upload
    This program checks divisibilty by 7.

---
+++ D7.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int n;
+	printf("Enter number: ");
+	scanf("%d",&n);
+	if (n%7=0)
+	{	printf("Number is divisible by 7.");
+	}
+	else
+	{	printf("Number is not divisible by 7.");
+	}
+	getch()
+}
